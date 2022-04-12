4c696c366f4fc6e47c8229a712112e6b5e53b67e
Tyler Robinson <tylerrhb11@gmail.com>
2022-04-12 11:35:35 -0400

    updating form and clock

--- reactbank/src/App.js
+++ reactbank/src/App.js
@@ -1,11 +1,41 @@
-import logo from './logo.svg';
+import {Component} from 'react'
 import './App.css';
 import Clock from './components/ClockComponent'
+import Form from './components/FormComponent';
 
-function App() {
-  return (
-    <Clock/>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      user: "Guest",
+      bgColor: "",
+      txtColor: ""
+    }
+  }
+
+  changeUser = (e) => {
+    e.preventDefault();
+
+    const currUser = e.target[0].value;
+    const currBgColor = e.target[1].value;
+    const currTxtColor = e.target[2].value;
+    console.log(currUser);
+    console.log(currBgColor);
+    console.log(currTxtColor);
+    this.setState({user: currUser})
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <Clock user={this.state.user} />
+        <h3>Customize Profile</h3>
+        <Form changeUser={this.changeUser} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
