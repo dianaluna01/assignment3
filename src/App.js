54f56faefbbbbe418d0224e45368ca567d7c2deb
Diana Luna <dianaluna0201@gmail.com>
2022-04-12 14:37:38 -0400

    second try at deploying to github pages

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import {Component} from 'react'
+import Clock from './components/ClockComponent'
+import List from './components/ListComponent'
+import Form from './components/FormComponent'
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      user: "Guest",
+      bgColor: "",
+      txtColor: ""
+    }
+  }
+
+  changeUser = (e) => {
+    e.preventDefault();
+
+    const currUser = e.target[0].value;
+    const currBgColor = e.target[1].value;
+    const currTxtColor = e.target[2].value;
+    console.log(currUser);
+    console.log(currBgColor);
+    console.log(currTxtColor);
+    this.setState({user: currUser})
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <Clock user={this.state.user} />
+        <List/>
+        <h3>Customize Profile</h3>
+        <Form changeUser={this.changeUser} />
+      </div>
+    );
+  }
+}
+
+export default App;
